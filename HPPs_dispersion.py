0d4531bce9cb7a0229915146c9f7fad8c09d350e
Office Lab <664688015@qq.com>
2022-03-11 11:36:39 +0800

    色散关系

---
+++ HPPs_dispersion.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import mv
+
+class HPPs_dispersion():
+    def __init__(self,filename):
+        self.name = filename
+
+    def ReadData(self):
+        data_file = os.path.join(fr'.\dispersion\{self.name}.txt')
+        data = pd.read_csv(data_file, sep='\s+', header=None)
+        # print(data[0])
+        return data
+
+    def Figure(self):
+        data = self.ReadData()
+        plt.figure(dpi=200)
+        plt.title(f'{self.name}_dispersion')
+        plt.style.use('seaborn-whitegrid')
+        plt.scatter(data[0], data[1],s=3)
+        # plt.show()
+    def savefig(self):
+        plt.savefig(fr'.\PNG\{self.name}_dispersion.png', dpi=200)
+# list = ['BN','BP','AlN','AlP','GaP']
+# for item in list:
+#     a1 =HPPs_dispersion(item)
+#     a1.ReadData()
+#     a1.Figure()
+#     a1.savefig()
+filename = 'GaP'
+newfilename = filename + '2'
+mv.rename(filename)
+
+a1 =HPPs_dispersion(newfilename)
+a1.ReadData()
+a1.Figure()
+a1.savefig()
